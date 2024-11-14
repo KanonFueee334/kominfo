a10df2ef77611fde013d90e9855919f257794245
DESKTOP-7B68CP2\DELL <nurdianws@gmail.com>
2024-11-14 16:14:39 +0700

    get user id from session

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -11,7 +11,7 @@ class AbsensiController extends Controller
 
     public function index()
     {
-        $userId = 3;
+        $userId = session('user_id');
 
         $absensiToday = DB::table(DB::raw("(SELECT time FROM `absensi` WHERE user_id = $userId AND DATE(time) = CURDATE()) as satu"))
                     ->select(DB::raw("DATE_FORMAT(MIN(satu.time),'%H:%i:%s') as masuk, DATE_FORMAT(MAX(satu.time),'%H:%i:%s') as pulang"))
@@ -28,7 +28,7 @@ class AbsensiController extends Controller
 
     public function saveAbsensi()
     {
-        $userId = 3;
+        $userId = session('user_id');
 
         $absensi = new Absensi();
         $absensi->user_id = $userId;
@@ -44,7 +44,7 @@ class AbsensiController extends Controller
 
     public function history()
     {
-        $userId = 3;
+        $userId = session('user_id');
 
         $absenList = Absensi::select(DB::raw("DATE_FORMAT(time, '%d-%m-%Y') as tgl, DATE_FORMAT(time, '%H:%i:%s') as waktu"))
                     ->where('user_id',$userId)
@@ -56,6 +56,7 @@ class AbsensiController extends Controller
 
     public function recap($dateStart, $dateEnd)
     {
+        $userId = session('user_id');
         $data = [];
 
         if(!empty($dateStart) && !empty($dateEnd)){
@@ -74,12 +75,12 @@ class AbsensiController extends Controller
             (SELECT IF(a.user_id IS NULL, b.user_id, a.user_id) AS user_id, IF(a.tgl IS NULL, b.leave_date, a.tgl) AS tgl_absen, a.masuk, a.pulang, a.terlambat, a.cepat_pulang, b.user_id AS usr_id, b.leave_date AS tgl_izin, b.type, b.note, b.letter
             FROM absensi_recap_view a
             LEFT JOIN izin b ON a.user_id = b.user_id AND STR_TO_DATE(a.tgl, '%d-%m-%Y') = b.leave_date
-            WHERE a.user_id = 3
+            WHERE a.user_id = ".$userId."
             UNION
             SELECT IF(a.user_id IS NULL, b.user_id, a.user_id) AS user_id, IF(a.tgl IS NULL, b.leave_date, a.tgl) AS tgl_absen, a.masuk, a.pulang, a.terlambat, a.cepat_pulang, b.user_id AS usr_id, b.leave_date AS tgl_izin, b.type, b.note, b.letter
             FROM absensi_recap_view a
             RIGHT JOIN izin b ON a.user_id = b.user_id AND STR_TO_DATE(a.tgl, '%d-%m-%Y') = b.leave_date
-            WHERE b.user_id = 3
+            WHERE b.user_id = ".$userId."
             ORDER BY tgl_absen ASC) absen
             ON cal.tgl = absen.tgl_absen;";
 
